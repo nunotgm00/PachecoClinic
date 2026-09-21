6c7a21ed9da13fc26abd0f63a73c6814e3a34dbf
Nuno Melanda <nunomelanda00@gmail.com>
2026-09-21 11:01:20 +0100

    Client Class

---
+++ PachecoClinic/Data/Entities/Client.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PachecoClinic.Data.Entities
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        [MaxLength(200)]
+        public string? Address { get; set; }
+
+
+        [MaxLength(20)]
+        public string? Nif { get; set; }
+
+
+        [Required]
+        public string UserId { get; set; }
+
+
+        public User User { get; set; }
+    }
+}
--- PachecoClinic/Data/Entities/User.cs
+++ PachecoClinic/Data/Entities/User.cs
@@ -1,8 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace PachecoClinic.Data.Entities
 {
-    public class User
+    public class User : IdentityUser
     {
         [MaxLength(50, ErrorMessage = "The field [0] only can contain {1} characters length.")]
         [Required]
