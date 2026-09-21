9016e66172100b4cbae066e95aa40f44a3be5217
Nuno Melanda <nunomelanda00@gmail.com>
2026-09-21 11:10:53 +0100

    Doctor Class

---
+++ PachecoClinic/Data/Entities/Doctor.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PachecoClinic.Data.Entities
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        public string UserId { get; set; }
+
+
+        public User User { get; set; }
+
+
+        [Required]
+        public int SpecialtyId { get; set; }
+
+
+        public Specialty Specialty { get; set; }
+
+
+        public bool Active { get; set; }
+    }
+}
