5dae18d116e0d9c626b99e7a8b36c8639dead9b1
Nuno Melanda <nunomelanda00@gmail.com>
2026-09-21 10:19:37 +0100

    Room Class

---
+++ PachecoClinic/Data/Entities/Room.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PachecoClinic.Data.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+
+
+        [Display(Name = "Room")]
+        [MaxLength(100, ErrorMessage = "The field {0} must have a maximum of {1} characters")]
+        [Required(ErrorMessage = "The field {0} is required")]
+        public string Name { get; set; }
+
+
+        [Display(Name = "Type")]
+        [MaxLength(100, ErrorMessage = "The field {0} must have a maximum of {1} characters")]
+        [Required(ErrorMessage = "The field {0} is required")]
+        public string Type { get; set; }
+
+
+        [Display(Name = "Under Maintenance")]
+        public bool UnderMaintenance { get; set; }
+    }
+}
