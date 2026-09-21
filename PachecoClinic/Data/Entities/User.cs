ea0767f46f7bcc7f1188b94971e7ef9308ec168a
Nuno Melanda <nunomelanda00@gmail.com>
2026-09-21 10:51:41 +0100

    User Class

---
+++ PachecoClinic/Data/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PachecoClinic.Data.Entities
+{
+    public class User
+    {
+        [MaxLength(50, ErrorMessage = "The field [0] only can contain {1} characters length.")]
+        [Required]
+        public string FirstName { get; set; }
+
+
+        [MaxLength(50, ErrorMessage = "The field [0] only can contain {1} characters length.")]
+        [Required]
+        public string LastName { get; set; }
+
+
+        public string? ImageUrl { get; set; }
+
+
+        [Display(Name = "Full Name")]
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
