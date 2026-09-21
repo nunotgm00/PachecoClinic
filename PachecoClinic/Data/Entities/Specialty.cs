323f76ab3a956cb20ae82566eadf3defb6093672
Nuno Melanda <nunomelanda00@gmail.com>
2026-09-21 10:04:41 +0100

    Specialty Class

---
+++ PachecoClinic/Data/Entities/Specialty.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PachecoClinic.Data.Entities
+{
+    public class Specialty
+    {
+        public int Id { get; set; }
+
+
+        [Display(Name = "Specialty")]
+        [MaxLength(100, ErrorMessage = "The field {0} must have a maximum of {1} characters")]
+        [Required(ErrorMessage = "The field {0} is required")]
+        public string Name { get; set; }
+    }
+}
